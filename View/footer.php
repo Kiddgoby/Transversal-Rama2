381bd1a9345de7d8eed8f49f735b05857879a01e
BielXandri <bielxandri@gmail.com>
2025-05-26 13:17:31 +0200

    Footer

---
+++ View/footer.php
@@ -0,0 +1,37 @@
+<footer>
+    <div class="footer.left"></div>
+  <p>Síguenos en redes:</p>
+  <p>
+    <p class="footer">Instagram</p> 
+    <p class="footer">Email</p> 
+    <p class="footer">TikTok</p>
+    <p class="footer">Tweeter</p>
+    <p class="footer">Tel: 984 03 87 11</p>
+  </p>
+  </div>
+
+<div class="footer.left"></div>
+  <p>Empresa</p>
+  <p>
+    <p class="footer">A cerca de Beat Pass</p> 
+    <p class="footer">Novedades</p> 
+    <p class="footer">Sostenivilidad</p>
+    <p class="footer">Proposito</p>
+    <p class="footer">Informar de un problema</p>
+  </p>
+  </div>
+
+  <div class="footer.left"></div>
+  <p>Ayuda</p>
+  <p>
+    <p class="footer">Obtener Ayuda</p> 
+    <p class="footer">Informacion del pedido</p> 
+    <p class="footer">Entradas y devoluciones</p>
+    <p class="footer">Opciones de pago</p>
+    <p class="footer">Contacto</p>
+  </p>
+  </div>
+
+  <p class="footer">&copy;2025 Beat Pass. Todos los derechos reservados.</p>
+</footer>
+
